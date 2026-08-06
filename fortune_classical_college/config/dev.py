e6d7e8ef397bcb9fb33bc57ac1f58d6ee600cb0f
frtFortune <frtfortuneiji@gmail.com>
2026-08-06 23:43:34 +0100

    Configure static files and build news listing

--- fortune_classical_college/config/dev.py
+++ fortune_classical_college/config/dev.py
@@ -1,13 +1,17 @@
 from .base import *
 from decouple import config
 
-# In development it's common to enable debug via env
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-# Use SQLite for development
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
